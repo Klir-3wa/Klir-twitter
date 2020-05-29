e32c1726f4f86beae57046d2606ef0a3e1e8c4b3
unknown <fmreda3@gmail.com>
2020-05-29 16:10:19 +0000

    Updates

---
+++ precache-manifest.de74c846eddf7731c8bc55bf33ee5fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450e4f4c15384eba9c14223cd503ae3e",
+    "url": "/Klir-twitter-host/index.html"
+  },
+  {
+    "revision": "cf5ebefef57185b90ac4",
+    "url": "/Klir-twitter-host/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "94f4119d99a0db82ff9f",
+    "url": "/Klir-twitter-host/static/css/main.e39df6e5.chunk.css"
+  },
+  {
+    "revision": "cf5ebefef57185b90ac4",
+    "url": "/Klir-twitter-host/static/js/2.f64f6163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Klir-twitter-host/static/js/2.f64f6163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f4119d99a0db82ff9f",
+    "url": "/Klir-twitter-host/static/js/main.127ae900.chunk.js"
+  },
+  {
+    "revision": "d3d6545bf76bc5f76a79",
+    "url": "/Klir-twitter-host/static/js/runtime-main.cc6c409a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Klir-twitter-host/static/media/logo.5d5d9eef.svg"
+  }
+]);
